b37a5bc2375cbb60db59f3c0d49c0f9591094802
ivannov <ivan_st_ivanov@yahoo.com>
2015-10-12 00:59:58 +0300

    Connect the application to a mysql container

---
+++ application/customizations/startapp.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+JBOSS_HOME=/opt/jboss/wildfly
+JBOSS_CLI=$JBOSS_HOME/bin/jboss-cli.sh
+
+function wait_for_server() {
+  until `$JBOSS_CLI -c ":read-attribute(name=server-state)" 2> /dev/null | grep -q running`; do
+    sleep 1
+  done
+}
+
+$JBOSS_HOME/bin/standalone.sh -b 0.0.0.0 -c standalone.xml &
+
+wait_for_server
+
+$JBOSS_CLI -c << EOF
+batch
+
+# Add MySQL module
+module add --name=com.mysql --resources=/opt/jboss/wildfly/customizations/mysql-connector.jar --dependencies=javax.api,javax.transaction.api
+
+# Add MySQL driver
+/subsystem=datasources/jdbc-driver=mysql:add(driver-name=mysql,driver-module-name=com.mysql,driver-xa-datasource-class-name=com.mysql.jdbc.jdbc2.optional.MysqlXADataSource)
+
+# Add the datasource
+data-source add --name=mysqlDS --driver-name=mysql --jndi-name=java:jboss/datasources/PredComposerMySQLDS --connection-url=jdbc:mysql://$DB_PORT_3306_TCP_ADDR:$DB_PORT_3306_TCP_PORT/sample --user-name=mysql --password=mysql --use-ccm=false --max-pool-size=25 --blocking-timeout-wait-millis=5000 --enabled=true
+
+# Execute the batch
+run-batch
+EOF
+
+echo "=> Shutting down WildFly"
+$JBOSS_CLI -c ":shutdown"
+
+cp /opt/jboss/wildfly/customizations/predcomposer.war /opt/jboss/wildfly/standalone/deployments
+
+echo "=> Restarting WildFly"
+$JBOSS_HOME/bin/standalone.sh -b 0.0.0.0
